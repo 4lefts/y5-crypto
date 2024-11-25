7e4b35af846f38c34b197e8302bf691afb4e652f
Stephen Ball <4lefts@users.noreply.github.com>
2024-11-25 16:45:23 +0000

    Update Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -31,7 +31,7 @@
   <nav>
     <!-- add class="not-yet" to these links to make inactive -->
     <a href="/#/caesar-cipher" style="--link-color: tomato">1. Caesar Cipher</a>
-    <a href="/#/substitution-cipher" style="--link-color: dodgerblue" class="not-yet"
+    <a href="/#/substitution-cipher" style="--link-color: dodgerblue"
       >2. Substitution Cipher</a
     >
     <a href="/#/frequency-analysis" style="--link-color: hotpink" class="not-yet"
