112ed11161cc06e1ef7aae840299fa766b6025ff
Stephen Ball <4lefts@users.noreply.github.com>
2025-11-11 16:38:26 +0000

    made password button active

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -33,9 +33,7 @@
     <a href="/#/caesar-cipher" style="--link-color: tomato">1. Caesar Cipher</a>
     <a href="/#/substitution-cipher" style="--link-color: dodgerblue">2. Substitution Cipher</a>
     <a href="/#/frequency-analysis" style="--link-color: hotpink">3. Frequency Analysis</a>
-    <a href="/#/password-cracking" style="--link-color: darkorchid" class="not-yet"
-      >4. Password Cracking</a
-    >
+    <a href="/#/password-cracking" style="--link-color: darkorchid">4. Password Cracking</a>
     <a href="/#/vocab" style="--link-color: teal">Useful Vocabulary</a>
   </nav>
 </main>
